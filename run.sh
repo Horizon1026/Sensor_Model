0e38f16f2cea410356fa99cf62a5fc3de8d2eb34
Horizon1026 <lijiapeng_atlantis@163.com>
2023-05-18 00:10:31 +0800

    add covisible graph

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd build/
 ./test_camera_model
 ./test_imu_model ../examples/imu_pose.txt
+./test_covisible_graph
 cd ..
---
+++ test/test_covisible_graph.cpp
@@ -0,0 +1,7 @@
+#include "log_report.h"
+
+int main(int argc, char **argv) {
+    ReportInfo(YELLOW ">> Test covisible graph." RESET_COLOR);
+
+    return 0;
+}
