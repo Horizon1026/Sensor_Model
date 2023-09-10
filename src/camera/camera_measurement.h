d29c7ffcd02721fa3eaaabce6a047e91bccd2a00
Horizon1026 <lijiapeng_atlantis@163.com>
2023-09-10 08:42:44 +0800

    update code

--- src/camera/camera_measurement.h
+++ src/camera/camera_measurement.h
@@ -2,12 +2,13 @@
 #define _SENSOR_MODEL_CAMERA_MEASUREMENT_H_
 
 #include "datatype_basic.h"
+#include "datatype_image.h"
 
 namespace SENSOR_MODEL {
 
 /* Measurement of camera. */
 struct CameraMeasurement {
-    MatImg image = MatImg::Zero(1, 1);
+    GrayImage image;
     float time_stamp_s = 0.0f;
 };
 
