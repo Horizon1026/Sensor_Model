9dd27e317be1892376a1f563c1f4f6ebaeb1689b
Horizon1026 <lijiapeng_atlantis@163.com>
2024-03-08 00:17:56 +0800

    update code

--- src/imu/imu_state.h
+++ src/imu/imu_state.h
@@ -13,6 +13,8 @@ enum ImuIndex : uint8_t {
     kBiasAccel = 9,
     kBiasGyro = 12,
     kGravity = 15,
+    kGyro = 18,
+    kAccel = 21,
 
     kNoiseAccel = 0,
     kNoiseGyro = 3,
@@ -32,8 +34,29 @@ class ImuState {
 
 public:
     ImuState() = default;
-    ImuState(const Vec3 &p_wi, const Quat &q_wi, const Vec3 &v_wi, const Vec3 &ba, const Vec3 &bg, const Vec3 &g_w, const float t) :
-        p_wi_(p_wi), q_wi_(q_wi), v_wi_(v_wi), ba_(ba), bg_(bg), g_w_(g_w), time_stamp_s_(t) {}
+    ImuState(const Vec3 &p_wi,
+             const Quat &q_wi,
+             const Vec3 &v_wi,
+             const Vec3 &ba,
+             const Vec3 &bg,
+             const Vec3 &g_w,
+             const float t) :
+        p_wi_(p_wi), q_wi_(q_wi), v_wi_(v_wi),
+        ba_(ba), bg_(bg), g_w_(g_w),
+        time_stamp_s_(t) {}
+    ImuState(const Vec3 &p_wi,
+             const Quat &q_wi,
+             const Vec3 &v_wi,
+             const Vec3 &ba,
+             const Vec3 &bg,
+             const Vec3 &g_w,
+             const Vec3 &gyro,
+             const Vec3 &accel,
+             const float t) :
+        p_wi_(p_wi), q_wi_(q_wi), v_wi_(v_wi),
+        ba_(ba), bg_(bg), g_w_(g_w),
+        gyro_(gyro), accel_(accel),
+        time_stamp_s_(t) {}
     virtual ~ImuState() = default;
 
     void Clear() {
@@ -43,8 +66,11 @@ public:
         ba_ = Vec3::Zero();
         bg_ = Vec3::Zero();
         g_w_ = Vec3(0, 0, 9.8f);
+        gyro_ = Vec3::Zero();
+        accel_ = Vec3::Zero();
+
         time_stamp_s_ = 0.0f;
-        cov_matrix_ = Mat18::Zero();
+        cov_matrix_ = Mat24::Zero();
     }
 
     // Reference for Member Variables.
@@ -54,9 +80,11 @@ public:
     Vec3 &ba() { return ba_; }
     Vec3 &bg() { return bg_; }
     Vec3 &g_w() { return g_w_; }
+    Vec3 &gyro() { return gyro_; }
+    Vec3 &accel() { return accel_; }
     float &time_stamp_s() { return time_stamp_s_; }
-    Mat18 &covariance() { return cov_matrix_; }
-    Mat18 &sqrt_cov() { return cov_matrix_; }
+    Mat24 &covariance() { return cov_matrix_; }
+    Mat24 &sqrt_cov() { return cov_matrix_; }
 
     // Const Reference for Member Variables.
     const Vec3 &p_wi() const { return p_wi_; }
@@ -65,9 +93,11 @@ public:
     const Vec3 &ba() const { return ba_; }
     const Vec3 &bg() const { return bg_; }
     const Vec3 &g_w() const { return g_w_; }
+    const Vec3 &gyro() const { return gyro_; }
+    const Vec3 &accel() const { return accel_; }
     const float &time_stamp_s() const { return time_stamp_s_; }
-    const Mat18 &covariance() const { return cov_matrix_; }
-    const Mat18 &sqrt_cov() const { return cov_matrix_; }
+    const Mat24 &covariance() const { return cov_matrix_; }
+    const Mat24 &sqrt_cov() const { return cov_matrix_; }
 
 private:
     Vec3 p_wi_ = Vec3::Zero();
@@ -76,11 +106,13 @@ private:
     Vec3 ba_ = Vec3::Zero();
     Vec3 bg_ = Vec3::Zero();
     Vec3 g_w_ = Vec3(0, 0, 9.8f);
+    Vec3 gyro_ = Vec3::Zero();
+    Vec3 accel_ = Vec3::Zero();
 
     float time_stamp_s_ = 0.0f;
 
     // Shared by covariance and sqrt_covariance.
-    Mat18 cov_matrix_ = Mat18::Zero();
+    Mat24 cov_matrix_ = Mat24::Zero();
 
 };
 
